77cbf32832e32d64be6bdd01243bab42bdd2ab22
Mamon Dehabra <dehabra.ma@gmail.com>
2022-02-26 15:00:58 +0100

    explaine_test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mlx-lab-backend
  Backend API with Django REST and TensorFlow
+
+### npm install tailwindcss@latest
--- api/views.py
+++ api/views.py
@@ -4,10 +4,24 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 import tensorflow as tf
+from django.http import HttpResponse, HttpResponseNotFound
 from tensorflow.keras import layers
 from tensorflow.keras import models
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.pipeline import make_pipeline
+
+import shap
+import matplotlib.pyplot as plt
+import mpld3
+from mpld3 import plugins
+from IPython.core.display import display, HTML
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+
 # Create your views here.
+
 
 @api_view(['GET', 'POST'])
 def build_model(request):
@@ -18,3 +32,47 @@ def build_model(request):
     model.build()
     result = model.to_json()
     return Response({result})
+
+
+@api_view(['GET'])
+def explaine_model(request):
+
+    X, y = load_diabetes(return_X_y=True)
+    features = load_diabetes()['feature_names']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42)
+
+    model = make_pipeline(
+        StandardScaler(),
+        MLPRegressor(hidden_layer_sizes=(5,), activation='logistic',
+                     max_iter=10000, learning_rate='invscaling', random_state=0)
+    )
+
+    model.fit(X_train, y_train)
+
+    explainer = shap.KernelExplainer(model.predict, X_train)
+
+    shap_values = explainer.shap_values(X_test, nsamples=100)
+
+    print(shap_values)
+
+    # shap_display_force = shap.force_plot(explainer.expected_value,
+    #                                      shap_values[0, :], X_test[0, :], feature_names=features)
+
+    shap_summary_plot = shap.summary_plot(shap_values, X_test, show=False)
+
+    # shap_dependence_plot = shap.dependence_plot(0, shap_values, X_test)
+
+    # shap_waterfall_plot = shap.waterfall_plot(shap_values, 10, show=True)
+
+    shap.initjs()
+
+    shap_html = f"<head>{shap.getjs()}</head><body>{shap_summary_plot}</body>"
+
+    # plot_summary_plot = shap.summary_plot(shap_values, X_test, show=False)
+
+    # savefig('test.svg', bbox_inches='tight') full size plot image
+    # shap.save_html("index.htm", shap_summary_plot)
+
+    return HttpResponse(shap_html)
